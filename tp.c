954e22e804e621242dd1cde56dcddbced1cdf1de
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2023-09-22 14:02:22 -0300

    salidas

--- tp.c
+++ tp.c
@@ -2,37 +2,38 @@
 #include <time.h>
 
 // Variables de control
-const int CANT_PUESTOS_A = 1, CANT_PUESTOS_B = 2;
+const int N = 1, M = 2;
 
 // Variables de tiempo
 time_t tpll, t;
+time_t tps_a[N], tps_b[M];
 
 // Acumuladores
 int SPS = 0, STA = 0, NT = 0;
 
 int main(){
     srand(time(NULL));
-    time_t tps_a[CANT_PUESTOS_A], tps_b[CANT_PUESTOS_B];
-    inicializarArrayTiempos(tps_a,CANT_PUESTOS_A);
-    inicializarArrayTiempos(tps_b,CANT_PUESTOS_B);
+    
+    inicializarArrayTiempos(tps_a,N);
+    inicializarArrayTiempos(tps_b,M);
 
-    time_t menor_tps_a = buscarMenorTiempo(tps_a, CANT_PUESTOS_A);
-    time_t menor_tps_b = buscarMenorTiempo(tps_b, CANT_PUESTOS_B);
+    time_t menor_tps_a = buscarMenorTiempo(tps_a, N);
+    time_t menor_tps_b = buscarMenorTiempo(tps_b, M);
 
     if(tiempoEsMenorOIgual(menor_tps_a,menor_tps_b)){
         if(tiempoEsMenor(menor_tps_a, tpll)){
-            // salida por A
+            salida_por_a();
         }
         else {
-            // llegada
+            llegada();
         }
     }
     else {
         if(tiempoEsMenor(menor_tps_b, tpll)){
-            // salida por B
+            salida_por_b();
         }
         else {
-            // llegada
+            llegada();
         }
     }
 
@@ -72,3 +73,14 @@ int tiempoEsMenor(time_t tiempo, time_t tiempo_a_comparar) {
     return difftime(tiempo, tiempo_a_comparar) < 0;
 }
 
+void salida_por_a(){
+
+}
+
+void salida_por_b(){
+
+}
+
+void llegada() {
+
+}
--- tp.h
+++ tp.h
@@ -7,3 +7,6 @@ void inicializarArrayTiempos(time_t *array, int longitud);
 time_t buscarMenorTiempo(time_t *array, int longitud);
 int tiempoEsMenorOIgual(time_t tiempo, time_t tiempo_a_comparar);
 int tiempoEsMenor(time_t tiempo, time_t tiempo_a_comparar);
+void salida_por_a();
+void salida_por_b();
+void llegada();
