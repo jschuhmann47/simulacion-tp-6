d190e5caf679ad099d99626131e889c417f5fede
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2023-09-22 08:20:19 -0300

    generate rand number

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Simulacion TP 6
+
+Para compilar en Linux:
+`gcc tp.c -o tp`
+
+En Windows (creo):
+`gcc tp.c -o tp.exe`
--- tp.c
+++ tp.c
@@ -1,1 +1,23 @@
+#include "tp.h"
 
+// Variables de control
+const int CANT_PUESTOS_A = 1, CANT_PUESTOS_B = 2;
+
+// Variables de tiempo
+const time_t TPLL, TPSA, TPSB, T;
+
+// Acumuladores
+const int SPS = 0, STA = 0, NT = 0;
+
+int main(){
+    srand(time(NULL));
+
+
+    printf("%f",generarNroRandom());
+
+    return 0;
+}
+
+double generarNroRandom() {
+    return (double)rand() / (double)RAND_MAX ;
+}
