844d0806ccc19ca2a11352aa3310e9631fdf1fb6
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2023-09-22 08:57:48 -0300

    add time comparators

--- tp.c
+++ tp.c
@@ -1,19 +1,40 @@
 #include "tp.h"
+#include <time.h>
 
 // Variables de control
 const int CANT_PUESTOS_A = 1, CANT_PUESTOS_B = 2;
 
 // Variables de tiempo
-const time_t TPLL, T;
+time_t tpll, t;
 
 // Acumuladores
-const int SPS = 0, STA = 0, NT = 0;
+int SPS = 0, STA = 0, NT = 0;
 
 int main(){
     srand(time(NULL));
     time_t tps_a[CANT_PUESTOS_A], tps_b[CANT_PUESTOS_B];
     inicializarArrayTiempos(tps_a,CANT_PUESTOS_A);
     inicializarArrayTiempos(tps_b,CANT_PUESTOS_B);
+
+    time_t menor_tps_a = buscarMenorTiempo(tps_a, CANT_PUESTOS_A);
+    time_t menor_tps_b = buscarMenorTiempo(tps_b, CANT_PUESTOS_B);
+
+    if(tiempoEsMenorOIgual(menor_tps_a,menor_tps_b)){
+        if(tiempoEsMenor(menor_tps_a, tpll)){
+            // salida por A
+        }
+        else {
+            // llegada
+        }
+    }
+    else {
+        if(tiempoEsMenor(menor_tps_b, tpll)){
+            // salida por B
+        }
+        else {
+            // llegada
+        }
+    }
 
     printf("%f",generarNroRandom());
 
@@ -31,3 +52,23 @@ void inicializarArrayTiempos(time_t *array, int longitud) {
     }
     return;
 }
+
+time_t buscarMenorTiempo(time_t *array, int longitud) {
+    time_t min = *array;
+    for (int i = 1; i < longitud; i++) {
+        if (*array < min){
+            min = *array;
+        }
+        array++;
+    }
+    return min;
+}
+
+int tiempoEsMenorOIgual(time_t tiempo, time_t tiempo_a_comparar) {
+    return difftime(tiempo, tiempo_a_comparar) <= 0;
+}
+
+int tiempoEsMenor(time_t tiempo, time_t tiempo_a_comparar) {
+    return difftime(tiempo, tiempo_a_comparar) < 0;
+}
+
--- tp.h
+++ tp.h
@@ -4,3 +4,6 @@
 
 double generarNroRandom();
 void inicializarArrayTiempos(time_t *array, int longitud);
+time_t buscarMenorTiempo(time_t *array, int longitud);
+int tiempoEsMenorOIgual(time_t tiempo, time_t tiempo_a_comparar);
+int tiempoEsMenor(time_t tiempo, time_t tiempo_a_comparar);
